a2ecc325a8b20eabed9d02f6525d2615c152bff5
pfilsx <pfilsx@gmail.com>
2017-06-25 10:12:15 +0400

    Application files

---
+++ Application.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @brief		VIP Members Application Class
+ * @author		<a href='https://github.com/pfilsx'>pfilsx</a>
+ * @copyright	(c) 2017 pfilsx
+ * @package		IPS Community Suite
+ * @subpackage	VIP Members
+ * @since		21 Jun 2017
+ * @version		
+ */
+ 
+namespace IPS\vipmembers;
+
+/**
+ * VIP Members Application Class
+ */
+class _Application extends \IPS\Application
+{
+}
---
+++ modules/admin/vipmembers/addvip.php
@@ -0,0 +1,106 @@
+<?php
+
+
+namespace IPS\vipmembers\modules\admin\vipmembers;
+
+use \IPS\vipmembers\VMember;
+use \IPS\Helpers\Form;
+use \IPS\Member\Group;
+use \IPS\Output;
+use \IPS\Member;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * addvip
+ */
+class _addvip extends \IPS\Dispatcher\Controller
+{
+	/**
+	 * Execute
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+		\IPS\Dispatcher::i()->checkAcpPermission( 'addvip_manage' );
+		parent::execute();
+	}
+
+	/**
+	 * ...
+	 *
+	 * @return	void
+	 */
+	protected function manage()
+	{
+        Output::i()->title = Member::loggedIn()->language()->addToStack('vipmembers_add');
+
+        $form = new Form;
+
+        $form->addHeader( 'vipmembers_add' );
+
+        $memberInput = new \IPS\Helpers\Form\Member('vipmembers_member', null, true);
+        $groupInput = new \IPS\Helpers\Form\Select( 'vipmembers_group', null, true, array(
+            'options' =>  Group::groups()
+        ));
+        $timeInput = new \IPS\Helpers\Form\Number( 'vipmembers_time', 1, true,
+            array( 'unlimited' => 0, 'unlimitedLang' => 'vipmembers_infinity', 'min' => 1));
+
+        $form->add($memberInput);
+        $form->add($groupInput);
+
+        $form->add($timeInput);
+
+        if ($values = $form->values())
+        {
+            if (($valResult = $this->_validate($values)) !== true){
+                $form->addMessage($valResult, 'ipsMessage ipsMessage_error');
+                $memberInput->setValue();
+                $groupInput->setValue();
+                $timeInput->setValue();
+            }
+            else if ($values['vipmembers_member']->member_group_id == $values['vipmembers_group']){
+                $form->addMessage('vipmembers_already_in_group', 'ipsMessage ipsMessage_error');
+            } else {
+                $member = $values['vipmembers_member'];
+
+                if (VMember::findByMember($member) != null){
+                    $form->addMessage('vipmembers_already_exists', 'ipsMessage ipsMessage_error');
+                } else {
+                    $promotion = new VMember();
+                    $promotion->member_id = $member->member_id;
+                    $promotion->old_group_id = $member->member_group_id;
+                    $promotion->promotion_ends = $values['vipmembers_time'] == 0
+                        ? null
+                        : date('Y-m-d', (strtotime(date('d.m.Y')) + $values['vipmembers_time']*24*60*60));
+                    $promotion->save();
+                    $member->member_group_id = $values['vipmembers_group'];
+                    $member->save();
+
+                    $form->addMessage('vipmembers_add_success', 'ipsMessage ipsMessage_success');
+                }
+            }
+        }
+
+        Output::i()->output = $form;
+	}
+
+	protected function _validate($values){
+	    if (!isset($values['vipmembers_member']->member_id)){
+	        return 'vipmembers_no_member_id';
+        }
+        if (!isset($values['vipmembers_group'])){
+	        return 'vipmembers_no_group_id';
+        }
+        if (!isset($values['vipmembers_time'])){
+            return 'vipmembers_no_time';
+        }
+        return true;
+    }
+}
